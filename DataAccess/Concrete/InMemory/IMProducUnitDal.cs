ac39f38c43daa1a217c3f3fe70cac48a3f4c9d05
samet44 <sametkibriz44@gmail.com>
2021-03-01 23:18:41 +0300

    Added Core and DataAccess layer

---
+++ DataAccess/Concrete/InMemory/IMProducUnitDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class IMProducUnitDal : IProductUnitDal
+    {
+        List<ProductUnit> _productUnits;
+        public IMProducUnitDal()
+        {
+            _productUnits = new List<ProductUnit>
+            {
+                new ProductUnit{ProductUnitId=1,ProductUnitName="Kutu"},
+                new ProductUnit{ProductUnitId=1,ProductUnitName="Şişe"}
+            };
+        }
+        public void Add(ProductUnit entity)
+        {
+            _productUnits.Add(entity);
+        }
+
+        public void Delete(ProductUnit entity)
+        {
+            ProductUnit productUnitToDelete = _productUnits.SingleOrDefault(p => p.ProductUnitId == entity.ProductUnitId);
+            _productUnits.Remove(productUnitToDelete);
+        }
+
+        public ProductUnit Get(Expression<Func<ProductUnit, bool>> filter)
+        {
+            return _productUnits.SingleOrDefault(p => p.ProductUnitName.Contains(filter.ToString()));
+        }
+
+        public List<ProductUnit> GetAll(Expression<Func<ProductUnit, bool>> filter = null)
+        {
+            return _productUnits;
+        }
+
+        public void Update(ProductUnit entity)
+        {
+            ProductUnit productUnitToUpdate = _productUnits.SingleOrDefault(p => p.ProductUnitId == entity.ProductUnitId);
+            productUnitToUpdate.ProductUnitId = entity.ProductUnitId;
+            productUnitToUpdate.ProductUnitName = entity.ProductUnitName;
+        }
+    }
+}

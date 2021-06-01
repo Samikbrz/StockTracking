0c6ca9edd18ce1c3b5540878811efd2ea3154b4a
samet44 <sametkibriz44@gmail.com>
2021-06-01 16:18:25 +0300

    generated store transfer dto

---
+++ Entities/DTOs/StoreTransferDto.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class StoreTransferDto : IDto
+    {
+        public int Id { get; set; }
+        public string TransferNo { get; set; }
+        public DateTime Date { get; set; }
+        public string SourceStoreName { get; set; }
+        public string SourceShelfName { get; set; }
+        public string SourceDrawerName { get; set; }
+        public string TergetStoreName { get; set; }
+        public string TargetShelfName { get; set; }
+        public string TargetDrawerName { get; set; }
+        public string UserName { get; set; }
+    }
+}

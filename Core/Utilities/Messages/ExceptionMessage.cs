0bf0771af10e4e6013f4e310bf5f354a86bf711b
samet44 <sametkibriz44@gmail.com>
2021-06-08 19:05:09 +0300

    added exception middleware

---
+++ Core/Utilities/Messages/ExceptionMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Messages
+{
+    public static class ExceptionMessage
+    {
+        public static string InternalServerError => "Bu işlemi yapmak için yetkiniz yok!.";
+    }
+}
--- Entities/Concrete/Proposal.cs
+++ Entities/Concrete/Proposal.cs
@@ -14,5 +14,6 @@ namespace Entities.Concrete
         public int CompanyId { get; set; }
         public int Count { get; set; }
         public int StockStoreId { get; set; }
+        public decimal ProposalPrice { get; set; }
     }
 }
